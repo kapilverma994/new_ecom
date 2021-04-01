8e6524b8bf99b5a8ed6a37399e266c637efe36ea
kapilverma123 <kapil.verma@adsandurl.com>
2021-04-01 18:07:56 +0530

    initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // dd(Hash::make('admin@123'));
+        return view('admin.login');
+    }
+    public function auth(Request $request){
+       $email=$request->email;
+       $password=$request->password;
+  $result=Admin::where(['email'=>$email,'password'=>$password])->first();
+if(isset($result->id)){
+$request->session()->put('ADMIN_LOGIN',TRUE);
+$request->session()->put('ADMIN_ID',$result->id);
+return redirect('admin/dashboard');
+}else{
+    return redirect()->back()->with('error','Invalid Details');
+}
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+       return view('admin.dashboard');  
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin  $admin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Admin $admin)
+    {
+        //
+    }
+}

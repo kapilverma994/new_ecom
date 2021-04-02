5970ac7e441baaf723f93487987b0b5175f21aed
kapilverma123 <kapil.verma@adsandurl.com>
2021-04-02 17:59:00 +0530

    upddate

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -9,7 +9,9 @@
 
 @if(Session::has('success'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-{{Session::get('success')}}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+    {{Session::get('success')}}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
 @endif
